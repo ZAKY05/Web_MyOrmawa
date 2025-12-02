5d59090494bf4b25d0df74bc59fe5c589bf77eaa
ZWALK880 <irulwalker880@gmail.com>
2025-12-02 13:37:35 +0700

    Update LoginFunction.php

--- Function/LoginFunction.php
+++ Function/LoginFunction.php
@@ -83,7 +83,7 @@ if (isset($_POST['login'])) {
 // Logout
 if (isset($_GET['logout'])) {
     session_destroy();
-    header("Location: /MyOrmawa/App/View/SuperAdmin/Login.php");
+    header("Location: ../App/View/SuperAdmin/Login.php");
     exit();
 }
 ?>
